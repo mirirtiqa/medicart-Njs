54cdda71503345b7bb9d3e2646cf0ed945cfbd2d
Abhimanyoo125 <125238624+Abhimanyoo125@users.noreply.github.com>
2024-08-27 18:21:45 +0530

    2nd commit

--- src/app/page.js
+++ src/app/page.js
@@ -3,11 +3,18 @@ import Hero from "@/components/Hero";
 import Blog from "@/components/BlogSection/Blog";
 import HeaderMenu from "@/components/Header/HeaderMenu";
 import Footer from "@/components/Footer/Footer";
+import Header from "@/components/Header1/page";
+// import TopCategories from "@/components/OffersAndTrending/TopCategories";
+import OffersAndTrending from "@/components/OffersAndTrending/page";
+import CardGrid from "@/components/Categories/page";
 export default function Home() {
   return (
     <main>
+      <Header/>
       <HeaderMenu/>
       <Hero/>
+      <CardGrid/>
+      <OffersAndTrending/>
       <Blog/>
       <Footer/>
     </main>

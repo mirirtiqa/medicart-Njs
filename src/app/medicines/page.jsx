0cb2391696f2411f4c6f8ef6a2f8b5ff214ae98d
Irtiqa <mirirtiqa66@gmail.com>
2024-09-06 16:33:36 +0530

    implemented function for data fetching from firestore

--- src/app/medicines/page.jsx
+++ src/app/medicines/page.jsx
@@ -3,7 +3,7 @@ import MedicineCard from "@/components/medicineCard";
 export default function Medicines() {
     return (
       <div>
-        <h1>Medicines</h1>
+        <h1>This is work in progress</h1>
         <MedicineCard/>
        
       </div>
--- src/components/medicineCard.jsx
+++ src/components/medicineCard.jsx
@@ -5,12 +5,12 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-
+import { useEffect, useState } from 'react';
+import { getDocs } from 'firebase/firestore';
 import styled from 'styled-components';
+import { medColRef,db } from '@/lib/firebase';
 
-const medicines = [
-    {name:'Paracetamol', price:'100',description:"Medicine for headache and pain releif"}
-]
+
 
 const StyledCard = styled(Card)`
   max-width: 345px;
@@ -26,24 +26,75 @@ const StyledTypography = styled(Typography)`
 `;
 
 export default function MedicineCard() {
+
+  const [allMedicines, setAllMedicines] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchAllMedicines = async () => {
+    setLoading(true);
+    try {
+      const querySnapshot = await getDocs(medColRef);
+      const results = [];
+      querySnapshot.forEach((doc) => {
+        results.push({ id: doc.id, ...doc.data() });
+      });
+  
+      setAllMedicines(results); 
+    } catch (error) {
+      console.error('Error fetching medicines:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchAllMedicines();
+  }, []);
+
+
+  console.log(allMedicines);
+  
+
+
+
+
+
   return (
-    <StyledCard>
-      <StyledCardMedia
-        image="/medicines/paracetamol.jpg"
-        title="paracetamol"
-      />
-      <CardContent>
-        <StyledTypography gutterBottom variant="h5" component="div">
-          {medicines[0].name}
-        </StyledTypography>
-        <StyledTypography variant="body2" sx={{ color: 'text.secondary' }}>
-          {medicines[0].description}
-        </StyledTypography>
-      </CardContent>
-      <CardActions sx={{gap:12}}>
-      <Typography variant="subtitle2"> {'\u20B9'} {medicines[0].price} MRP</Typography>
-        <Button size="small">Add to Cart</Button>
-      </CardActions>
-    </StyledCard>
+
+    <>
+     {loading ? (
+        <Typography>Loading...</Typography>
+      ) : (
+        allMedicines.map((medicine) => (
+          <StyledCard key={medicine.id}>
+            <StyledCardMedia
+              image="/medicines/paracetamol.jpg" 
+              title={medicine.name}
+            />
+            <CardContent>
+              <StyledTypography gutterBottom variant="h5" component="div">
+                {medicine.name}
+              </StyledTypography>
+              <StyledTypography variant="body2" sx={{ color: 'text.secondary' }}>
+                {medicine.description}
+              </StyledTypography>
+            </CardContent>
+            <CardActions sx={{ gap: 12 }}>
+              <Typography variant="subtitle2">
+                {'\u20B9'} {medicine.price} MRP
+              </Typography>
+              <Button size="small">Add to Cart</Button>
+            </CardActions>
+          </StyledCard>
+        ))
+      )}
+    
+    
+    </>
+
+
+
+
+
   );
 }
